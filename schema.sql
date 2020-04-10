3c40aa773777d2dc9fa8f6273a02a92b857e9bef
unknown <mars.jones34@gmail.com>
2020-04-10 01:16:16 -0700

    initial commit. ... maybe?

--- schema.sql
+++ schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE role (
   salary DECIMAL(10,2) NOT NULL,
   department_id INT,
   PRIMARY KEY (id),
-  FOREIGN KEY (department_id)
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -26,6 +26,6 @@ CREATE TABLE employee (
   role_id INT,
   manager_id INT,
   PRIMARY KEY (id),
-  FOREIGN KEY (role_id),
-  FOREIGN KEY (manager_id)
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
